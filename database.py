2c85ae7aee180c62dbee0c80486216728ef0592d
f1sheater <hsulin22@student.oulu.fi>
2026-02-07 15:29:44 +0200

    Add functions

--- database.py
+++ database.py
@@ -49,3 +49,84 @@ class User(db.Model):
     comments = db.relationship("Comment", back_populates="user")
     upvotes = db.relationship("Upvote", back_populates="user", cascade="all, delete-orphan")
     reports = db.relationship("Report", back_populates="user")
+
+def init_db(clear=False):
+    with app.app_context():
+        if clear:
+            db.drop_all()
+        db.create_all()
+
+def add_report(user_id: int, description: str, location: str, type_id: int):
+    with app.app_context():
+        user = User.query.filter_by(id=user_id).first()
+        report_type = ReportType.query.filter_by(id=type_id).first()
+
+    new_report = Report(
+            user = user,
+            description = description,
+            location = location,
+            report_type = report_type,
+        )
+
+    with app.app_context():
+        db.session.add(new_report)
+        db.session.commit()
+
+def add_report_type(name: str, description: str=None):
+    new_report_type = ReportType(
+            name = name,
+            description = description,
+        )
+
+    with app.app_context():
+        db.session.add(new_report_type)
+        db.session.commit()
+
+def add_upvote(user_id: int, report_id: int):
+    with app.app_context():
+        user = User.query.filter_by(id=user_id).first()
+        report = Report.query.filter_by(id=report_id).first()
+
+    new_upvote = Upvote(
+            user = user,
+            report = report,
+        )
+
+    with app.app_context():
+        db.session.add(new_upvote)
+        db.session.commit()
+
+def add_comment(user_id: int, report_id: int, text: str):
+    with app.app_context():
+        user = User.query.filter_by(id=user_id).first()
+        report = Report.query.filter_by(id=report_id).first()
+
+    new_comment = Comment(
+            user = user,
+            report = report,
+            text = text,
+        )
+
+    with app.app_context():
+        db.session.add(new_comment)
+        db.session.commit()
+
+def add_user(name: str):
+    new_user = User(
+            name = name,
+        )
+
+    with app.app_context():
+        db.session.add(new_user)
+        db.session.commit()
+
+def delete_entry(table, id: int):
+    with app.app_context():
+        entry = table.query.filter_by(id=id).first()
+        if entry:
+            db.session.delete(entry)
+            db.session.commit()
+
+init_db(True)
+add_user("buh")
+delete_entry(User, 2)
