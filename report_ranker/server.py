4b472427ca2e9be32a5e3e627f657e5df9b7ad97
Kamppix <kimmo.leukkunen@gmail.com>
2026-04-26 16:58:07 +0300

    Change urgency calculation once again

--- report_ranker/server.py
+++ report_ranker/server.py
@@ -34,7 +34,7 @@ class RankingService(pb2_grpc.RankingServiceServicer):
 
         age_in_seconds = max(1, current_time - report_time)
 
-        score = (age_in_seconds / 86400 + comments / 4) * (1.2 ** upvotes)
+        score = upvotes / (age_in_seconds / 86400 + comments / 4)
         return float(score)
 
     def CalculateRanking(self, request, context):
